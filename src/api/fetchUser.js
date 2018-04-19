3bc8c8a49f70358e20ddedfcb2cec72c12aca4e7
Tigran Petrossian <tgpett@gmail.com>
2018-04-19 06:10:51 +0400

    Handle wrong IDs for when fetching users

--- src/api/fetchUser.js
+++ src/api/fetchUser.js
@@ -12,14 +12,20 @@ export function fetchUser(id) {
   console.log(`Fetching user ${id}`);
 
   return fetch(`user/${id}`)
-    .then(user => ({
-      username: user.id,
-      about: user.about || null,
-      createdAt: user.created,
-      delay: user.delay || null,
-      karma: user.karma || null,
-      submissions: user.submitted || null,
-    }));
+    .then(user => {
+      if (user === null) {
+        return null;
+      }
+
+      return ({
+        username: user.id,
+        about: user.about || null,
+        createdAt: user.created,
+        delay: user.delay || null,
+        karma: user.karma || null,
+        submissions: user.submitted || null,
+      });
+    });
 }
 
 export default fetchUser;
